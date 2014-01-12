765f46810888cfa6785898d5c6517c721a3a4a53
Gord <michaeljohngordon@gmail.com>
2014-01-12 10:25:21 -0500

    correcting test 2 and asserting text contains hello world

--- app.js
+++ app.js
@@ -4,18 +4,21 @@ var http = require('http');
 app = module.exports = express();
 
 app.use(express.logger());
-app.configure(function(){
-    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+app.configure(function() {
+    app.use(express.errorHandler({
+        dumpExceptions: true,
+        showStack: true
+    }));
 });
 
-app.get('/', function(req, res){
-  res.send('hello world');
+app.get('/', function(req, res) {
+    res.send('hello world');
 });
 
 if (!module.parent) {
     app.set('env', process.env.NODE_ENV || 'development');
-    app.set('port', process.env.PORT || 80); 
-    app.listen(app.get('port'), '0.0.0.0', function () {
+    app.set('port', process.env.PORT || 80);
+    app.listen(app.get('port'), '0.0.0.0', function() {
         console.log("Express server listening on port %d in %s mode", app.get('port'), app.get('env'));
     });
 }
--- test/app_test.js
+++ test/app_test.js
@@ -1,64 +1,62 @@
-var app  = require('../app');
+var app = require('../app');
 var superagent = require('superagent');
 var should = require('should');
 var http = require('http');
 
-describe('app', function(){
+describe('app', function() {
 
-// Before tests start up the app
+    // Before tests start up the app
 
-  before (function (done) {
-      
-    // Set application environment name and port  
-    app.set('env', 'test');
-    app.set('port', 3333);
-    
-    // Start app
-    app.listen(app.get('port'), '0.0.0.0', (function (err) {
-      if (err) {
-        console.log("Error starting the server");
-        done(err);
-      } 
-      else {
-        console.log("Express server listening on port %d in %s mode", app.get('port'), app.get('env'));
-        done();
-      }
-    }));
-  });
- 
-// Tests
- 
-  it('Test 1 - App should exist', function (done) {
-    should.exist(app);
-    done();
-  });
- 
-  it('Test 2 - App should be listening at localhost:3333', function (done) {
-    request
-       .get('http://localhost:3333/')
-       .end(function(res){
-           res.should.exist;
-           res.statusCode.should.eql(200);
-           res.text.should.contain('hello world');
-       });
-      done();
-  });
-  
-  
-// After tests close down the app
+    before(function(done) {
 
-  after(function (done) {
-    console.log("Stopping the server");
-    app.close(function (err) {
-      if (err) {
-          console.log("Error starting the server");
-          done(err);
-      }
-      else {
-        console.log("Stopped the server");
-        done();
-      }
+        // Set application environment name and port  
+        app.set('env', 'test');
+        app.set('port', 3333);
+
+        // Start app
+        app.listen(app.get('port'), '0.0.0.0', (function(err) {
+            if (err) {
+                console.log("Error starting the server");
+                done(err);
+            }
+            else {
+                console.log("Express server listening on port %d in %s mode", app.get('port'), app.get('env'));
+                done();
+            }
+        }));
     });
-  });
-  
+
+    // Tests
+
+    it('Test 1 - App should exist', function(done) {
+        should.exist(app);
+        done();
+    });
+
+    it('Test 2 - App should be listening at localhost:3333', function(done) {
+        superagent.get('http://localhost:3333/').end(function(res) {
+            res.should.exist;
+            res.statusCode.should.eql(200);
+            res.text.should.contain('hello world');
+        });
+        done();
+    });
+
+
+    // After tests close down the app
+
+    after(function(done) {
+        console.log("Stopping the server");
+        app.close(function(err) {
+            if (err) {
+                console.log("Error starting the server");
+                done(err);
+            }
+            else {
+                console.log("Stopped the server");
+                done();
+            }
+        });
+    });
+
 });
