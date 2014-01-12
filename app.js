9337f5f97fe5cf074493b10faed062b41aae9fc0
Gord <michaeljohngordon@gmail.com>
2014-01-12 07:56:45 -0500

    move routing

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ app.configure(function(){
     app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 
+app.get('/', function(req, res){
+  res.send('hello world');
+});
+
 startServer(80);
 
 // Function to start server on any port passed to it
@@ -15,9 +19,5 @@ function startServer (portNumber) {
     console.log('Port should be ' + portNumber);
     app.listen(portNumber);
     console.log('Listening on port ' + app.address.port);
-    app.close();
 }
 
-app.get('/', function(req, res){
-  res.send('hello world');
-});
