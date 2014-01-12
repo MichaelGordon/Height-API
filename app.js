66666fc04e7046cc5746ea40f795cc380c747f06
Gord <michaeljohngordon@gmail.com>
2014-01-12 06:39:52 -0500

    changed start up again

--- app.js
+++ app.js
@@ -4,25 +4,19 @@ var http = require('http');
 var app = module.exports = express();
 app.use(express.logger());
 
-var server = http.createServer(app);
+// Function to start server on any port passed to it
+
+function startServer (port) {
+    var server = http.createServer(app);
+    server.listen(port);
+    console.log(server.address);
+    console.log(server.settings.env);
+    console.log('Listening on port ' + server.address().port);
+    server.close();
+}
+
+startServer(80);
 
 app.get('/', function(req, res){
   res.send('hello world');
 });
-
- 
-
-server.listen(80);
-console.log('Listening on port ' + server.address().port);
-server.close();
- 
-// Make sure app is not started on default port if part of test suite
-
-if (!module.parent) {
-  app.listen(port, function () {
-    console.log("Express server listening on port %d in %s mode",
-    app.address().port,
-    app.settings.env
-  );
-});
-};
