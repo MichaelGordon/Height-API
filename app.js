bf9a873fb6674a8534cb9c59c276511a4ba18676
Gord <michaeljohngordon@gmail.com>
2014-01-12 09:30:45 -0500

    on test start app on port 3333

--- app.js
+++ app.js
@@ -14,16 +14,13 @@ app.get('/', function(req, res){
 
 if (!module.parent) {
     app.listen(app.get('port'), function () {
-        console.log("Express server listening on port %d in %s mode",
-        app.get('port'),
-        app.get('env'));
+        console.log("Express server listening on port %d in %s mode", app.get('port'), app.get('env'));
     });
 }
 else {
     app.set('env', process.env.NODE_ENV || 'development');
     app.set('port', process.env.PORT || 80);    
-    app.listen(app.get('port'));
-    console.log("Express server listening on port %d in %s mode",
-    app.get('port'),
-    app.get('env'));
+    app.listen(app.get('port'), function() {
+        console.log("Express server listening on port %d in %s mode", app.get('port'), app.get('env'));
+    });
 }
