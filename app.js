9ffa42de7829d1898f5585316ba8cbd5866e91cb
Gord <michaeljohngordon@gmail.com>
2014-01-13 19:06:32 -0500

    Made basic api pass/fail methods and parameters with correct HTTP codes and JSON messages with tests

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var express = require('express');
-var http = require('http');
 
-app = module.exports = express();
+var app = module.exports = express();
 
 app.use(express.logger());
 app.configure(function() {
@@ -11,14 +10,41 @@ app.configure(function() {
     }));
 });
 
-app.get('/', function(req, res) {
-    res.send('hello world');
+// Nearest resource
+
+// Accept all methods
+app.all('/nearest', function(req, res) {
+
+    // If the method is GET then do something with it
+    if (req.method === 'GET') {
+
+        // If location parameter has been sent then return something
+        if (req.query.location) {
+            res.send(200, {
+                'location': 'something'
+            });
+        }
+        // If location paramter is missing then give correct HTTP code and informative JSON message
+        else {
+            res.send(400, {
+                'error': 'Missing parameter - location'
+            });
+        }
+    }
+    // Any other method give correct HTTP code and informative JSON message
+    else {
+        res.send(405, {
+            'error': 'Method not allowed'
+        });
+    }
 });
+
+// Allow app to be started up externally by test but if not start it in development mode
 
 if (!module.parent) {
     app.set('env', process.env.NODE_ENV || 'development');
     app.set('port', process.env.PORT || 80);
-    app.listen(app.get('port'), '0.0.0.0', function() {
+    app.listen(app.get('port'), process.env.IP || '0.0.0.0', function() {
         console.log("Express server listening on port %d in %s mode", app.get('port'), app.get('env'));
     });
 }
--- test/app_test.js
+++ test/app_test.js
@@ -1,27 +1,33 @@
+// Requires
+
 var app = require('../app');
 var should = require('should');
 var superagent = require('superagent');
-var domain = ('http://' + process.env.IP + ':' + process.env.PORT);
-var server;
 
-describe('App Testing', function() {
+// Set up configs for app in test environment
+
+app.set('env', 'test');
+app.set('port', process.env.PORT || 3333);
+var port = app.get('port');
+var env = app.get('env');
+var ip = process.env.IP;
+var server;
+var domain = ('http://' + ip + ':' + port);
+
+describe('Testing -', function() {
 
     // Before tests start up the app
 
     before(function(done) {
 
-        // Set application environment name and port  
-        app.set('env', 'test');
-        app.set('port', process.env.PORT || 3333);
-
         // Start app
-        server = app.listen(app.get('port'), process.env.IP, (function(err) {
+        server = app.listen(port, ip, (function(err) {
             if (err) {
                 console.log("Error starting the server");
                 done(err);
             }
             else {
-                console.log("Express server listening on port %d in %s mode", app.get('port'), app.get('env'));
+                console.log("Express server listening on port %d in %s mode on %s", port, env, ip);
                 done();
             }
         }));
@@ -34,22 +40,102 @@ describe('App Testing', function() {
         done();
     });
 
-    it('Test 2 - App should be listening at ' + process.env.IP + ':' + app.get('port') + '/ and returning status code 200', function(done) {
-        superagent.get(domain + '/').end(function(res) {
-            res.should.exist;
-            res.statusCode.should.eql(200);
+
+    describe('App methods', function() {
+
+        it('Test 1 - GET - /nearest should exist and return 400', function(done) {
+            var agent = superagent.agent();
+            agent.get(domain + '/nearest').end(function(err, res) {
+                should.not.exist(err);
+                res.should.exist;
+                res.should.have.status(400);
+                done();
+            });
         });
-        done();
+
+        it('Test 2 - GET - /nearest returns json exception - missing parameter', function(done) {
+            var agent = superagent.agent();
+            agent.get(domain + '/nearest').end(function(err, res) {
+                should.not.exist(err);
+                res.should.exist;
+                res.should.be.json;
+                res.body.should.have.property('error', 'Missing parameter - location');
+                done();
+            });
+        });
+
+        it('Test 3 - POST - /nearest should exist and return 405', function(done) {
+            var agent = superagent.agent();
+            agent.post(domain + '/nearest').end(function(err, res) {
+                should.not.exist(err);
+                res.should.exist;
+                res.statusCode.should.eql(405);
+                done();
+            });
+        });
+
+        it('Test 4 - POST - /nearest returns json exception - method not allowed', function(done) {
+            var agent = superagent.agent();
+            agent.post(domain + '/nearest').end(function(err, res) {
+                should.not.exist(err);
+                res.should.exist;
+                res.should.be.json;
+                res.body.should.have.property('error', 'Method not allowed');
+                done();
+            });
+        });
+
+        it('Test 5 - DELETE /nearest should exist and return 405', function(done) {
+            var agent = superagent.agent();
+            agent.del(domain + '/nearest').end(function(err, res) {
+                should.not.exist(err);
+                res.should.exist;
+                res.statusCode.should.eql(405);
+                done();
+            });
+        });
+
+        it('Test 6 - DELETE - /nearest returns json exception - method not allowed', function(done) {
+            var agent = superagent.agent();
+            agent.del(domain + '/nearest').end(function(err, res) {
+                should.not.exist(err);
+                res.should.exist;
+                res.should.be.json;
+                res.body.should.have.property('error', 'Method not allowed');
+                done();
+            });
+        });
     });
 
-    it('Test 3 - / should contain hello world text', function(done) {
-        superagent.get(domain + '/').end(function(res) {
-            res.text.should.contain('hello world');
+    describe('App parameters', function() {
+
+        it('Test 1 - GET - /nearest?location=something returns something', function(done) {
+            var agent = superagent.agent();
+            agent.get(domain + '/nearest?location=something').end(function(err, res) {
+                should.not.exist(err);
+                res.should.exist;
+                res.statusCode.should.eql(200);
+                res.should.be.json;
+                res.body.should.have.property('location', 'something');
+                done();
+            });
         });
-        done();
     });
+    /*
+        it('Test 6 - /nearest should return 400 for any other parameter', function(done) {
+            var agent = superagent.agent();
+            agent.get(domain + '/nearest?badparameter=bad').end(onResponse);
 
-
+            function onResponse(err, res) {
+                err.should.not.exist;
+                res.should.exist;
+                res.statusCode.should.eql(400);
+                console.log(res.statusCode);
+                return done();
+            }
+        });
+    });
+*/
     // After tests close down the app
 
     after(function(done) {
@@ -65,5 +151,5 @@ describe('App Testing', function() {
             }
         });
     });
-    
+
 });
